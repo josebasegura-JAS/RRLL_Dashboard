7f132bfbfe13dee7a72f24093769bfcb9d21a4e5
josebasegura-JAS <josebasegura@gmail.com>
2026-05-20 13:35:54 +0200

    Add files via upload

--- preload.js
+++ preload.js
@@ -14,3 +14,21 @@ contextBridge.exposeInMainWorld("rrllDB", {
   importParitariaHistoryDocx: () => ipcRenderer.invoke("db:importParitariaHistoryDocx"),
   generateCommitteeMinutesDraft: payload => ipcRenderer.invoke("db:generateCommitteeMinutesDraft", payload)
 });
+
+contextBridge.exposeInMainWorld("rrllTicketRestaurant", {
+  importSpreadsheet: () => ipcRenderer.invoke("ticketRestaurant:importSpreadsheet"),
+  exportWorkbook: payload => ipcRenderer.invoke("ticketRestaurant:exportWorkbook", payload)
+});
+
+contextBridge.exposeInMainWorld("rrllFolder", {
+  getPath: () => ipcRenderer.invoke("rrllFolder:getPath"),
+  setPath: folderPath => ipcRenderer.invoke("rrllFolder:setPath", folderPath),
+  open: () => ipcRenderer.invoke("rrllFolder:open")
+});
+
+
+contextBridge.exposeInMainWorld("rrllAttachments", {
+  selectFiles: () => ipcRenderer.invoke("attachments:selectFiles"),
+  openPath: filePath => ipcRenderer.invoke("attachments:openPath", filePath),
+  openFolder: filePath => ipcRenderer.invoke("attachments:openFolder", filePath)
+});
